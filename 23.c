634aed251dda102ad90bfe2d1148308e120137ac
sktperiasamy <31505529+sktperiasamy@users.noreply.github.com>
2018-03-19 16:58:28 +0530

    Create 23.c

---
+++ 23.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main()
+{
+	int m1,m2,m3,h1,h2,h3;
+	printf(" enter the values:");
+	scanf("%d%d%d%d",&m1,&m2,&h1,&h2);
+	h3=h1-h2;
+	m3=m1-m2;
+	printf("%d%d",h3,m3);
+	return 0;
+ 
+ 
+}
